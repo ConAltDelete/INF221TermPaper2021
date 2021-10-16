ad081871e38d0eebec74bc4360eff4b42dbd2d70
Roy <roygra@nmbu.no>
2021-10-16 03:31:36 +0200

    med3 quicksort fully works

--- code/quicksort_med3.py
+++ code/quicksort_med3.py
@@ -1,11 +1,13 @@
 def partision(A, p, r):
-    x = med3(A, p, int((p+r)/2), r)
+    pid = med3(A, p, int((p+r)/2), r)
+    x = A[pid]
+    A[pid], A[r] = A[r], A[pid]
     i = p - 1
-    for j in range(p, r + 1):
+    for j in range(p, r):
         if A[j] <= x:
             i += 1
             A[j], A[i] = A[i], A[j]
-    A[i], A[p] = A[p], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
     return i + 1
 
 
@@ -22,17 +24,25 @@ def quicksort(A: list, p=None, r=None):
 
 def med3(A, p, m, r):
     lo, mid, hi = A[p], A[m], A[r]
+    swap = 0
     if lo > hi:
         hi, lo = lo, hi
+        swap += 1
     if mid < lo:
-        return lo
+        if swap == 1:
+            return p
+        else:
+            return r
     elif mid > hi:
-        return hi
+        if swap == 1:
+            return r
+        else:
+            return p
     else:
-        return mid
+        return m
 
 
 if __name__ == "__main__":
-    L = [4, 5, 2, 3, 1]
+    L = [5, 4, 1, 2, 3]
     quicksort(L,0,len(L)-1)
     print(L)
