c2de90cb2b7466a62133522104dbb0f73e52d832
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2021-10-24 12:34:10 +0200

    updated pdfs in folder

--- code/R/csv2plot.R
+++ code/R/csv2plot.R
@@ -13,21 +13,21 @@ for (file in list_files) {
 	pt <- 0.95
 	
 	pdf(paste("./data/pics/",file_outhead , "_sorted.pdf",sep=""))
-	plot(x=data_file$"lg2.n",y=data_file$"sorted",type="l",xlab="lg2.n",ylab="time in sec",main=paste("plot of sorted" , file_outhead), ylim = c(0,1.2*max(data_file$"max_sort")))
+	plot(x=data_file$"lg2.n",y=data_file$"sorted",type="l",xlab="lg2.n",ylab="time in sec",main=paste("plot of sorted" , file_outhead), ylim = c(0,1.2*max(data_file$"sorted" +qnorm(pt)*sqrt(data_file$"sorted.variance"))))
 	points(x=data_file$"lg2.n",y=data_file$"min_sort", pch=6)
 	points(x=data_file$"lg2.n", y=data_file$"max_sort", pch=2)
 	polygon(x=c(data_file$"lg2.n",rev(data_file$"lg2.n")),y=c(data_file$"sorted" + qnorm(pt)*sqrt(data_file$"sorted.variance"),rev(data_file$"sorted" - qnorm(pt)*sqrt(data_file$"sorted.variance"))), col = "#E60026", density= 10, angle = 45 )
 	dev.off()
 
 	pdf(paste("./data/pics/",file_outhead , "_reversed.pdf",sep=""))
-	plot(x=data_file$"lg2.n",y=data_file$"reversed",type="l",xlab="lg2.n",ylab="time in sec",main=paste("plot of reversed" , file_outhead), ylim = c(0,1.2*max(data_file$"max_rev")))
+	plot(x=data_file$"lg2.n",y=data_file$"reversed",type="l",xlab="lg2.n",ylab="time in sec",main=paste("plot of reversed" , file_outhead), ylim = c(0,1.2*max(data_file$"reversed" +qnorm(pt)*sqrt(data_file$"reversed.variance"))))
 	points(x=data_file$"lg2.n",y=data_file$"min_rev", pch=6)
 	points(x=data_file$"lg2.n", y=data_file$"max_rev", pch=2)
 	polygon(x=c(data_file$"lg2.n",rev(data_file$"lg2.n")),y=c(data_file$"reversed" + qnorm(pt)*sqrt(data_file$"reversed.variance"),rev(data_file$"reversed" - qnorm(pt)*sqrt(data_file$"reversed.variance"))), col = "#E60026", density= 10, angle = 45 )
 	dev.off()
 
 	pdf(paste("./data/pics/",file_outhead , "_random.pdf",sep=""))
-	plot(x=data_file$"lg2.n",y=data_file$"random",type="l",xlab="lg2.n",ylab="time in sec",main=paste("plot of random" , file_outhead), ylim = c(0,1.2*max(data_file$"max_rand")))
+	plot(x=data_file$"lg2.n",y=data_file$"random",type="l",xlab="lg2.n",ylab="time in sec",main=paste("plot of random" , file_outhead), ylim = c(0,1.2*max(data_file$"random" +qnorm(pt)*sqrt(data_file$"random.variance"))))
 	points(x=data_file$"lg2.n",y=data_file$"min_rand", pch=6)
 	points(x=data_file$"lg2.n", y=data_file$"max_rand", pch=2)
 	polygon(x=c(data_file$"lg2.n",rev(data_file$"lg2.n")),y=c(data_file$"random" + qnorm(pt)*sqrt(data_file$"random.variance"),rev(data_file$"random" - qnorm(pt)*sqrt(data_file$"random.variance"))), col = "#E60026", density= 10, angle = 45 )
