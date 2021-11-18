ec3f88abaf26dceca9c11e641c3d73b6c447882a
Sindre Stokke <sindre.stokke@nmbu.no>
2021-11-18 19:26:33 +0100

    Added plotting for best case and worst case

--- code/python/plots.py
+++ code/python/plots.py
@@ -11,45 +11,70 @@ import numpy as np
 
 folder = "../../data/csv_files_roy/"
 
-data_bubble_sort = pd.read_csv(folder+"bobble_sort.csv")
-#data_cyclesort = pd.read_csv(folder+"cyclesort.csv")
-data_insertion_sort = pd.read_csv(folder+"insertion_sort.csv")
-data_mergesort = pd.read_csv(folder+"mergesort.csv")
-data_mergesort_insert = pd.read_csv(folder+"mergesort_insert.csv")
-data_numpy_sort = pd.read_csv(folder+"numpy_sort.csv")
-data_python_sort = pd.read_csv(folder+"python_sort.csv")
-#data_quicksort = pd.read_csv(folder+"quicksort.csv")
-data_quicksort_insert = pd.read_csv(folder+"quicksort_insert.csv")
-#data_quicksort_med3 = pd.read_csv(folder+"quicksort_med3.csv")
-data_quicksortiterative = pd.read_csv(folder+"quicksortiterative.csv")
+data = [pd.read_csv(folder+"bobble_sort.csv"),
+        pd.read_csv(folder+"insertion_sort.csv"),
+        pd.read_csv(folder+"mergesort.csv"),
+        pd.read_csv(folder+"mergesort_insert.csv"),
+        pd.read_csv(folder+"numpy_sort.csv"),
+        pd.read_csv(folder+"python_sort.csv"),
+        pd.read_csv(folder+"quicksort_insert.csv"),
+        pd.read_csv(folder+"quicksortiterative.csv")]
+
+data_labels = ["Bubble sort", "Insertion sort", "Mergesort", "Mergesort insert",
+               "Numpy sort", "Python sort", "Quicksort insert", "Quicksort iterative"]
 
 
-def plot_time_datasize(list_order):
-    xpoints = data_bubble_sort["lg2 n"].values
+def plot_time_datasize(data, data_labels, list_order):
+    xpoints = data[0]["lg2 n"].values
     
     figur = plt.figure()
     akser = figur.add_subplot()
     
-    akser.plot(xpoints, data_bubble_sort[list_order].values, label="Bubble sort")
-    akser.plot(xpoints, data_insertion_sort[list_order].values, label="Insertion sort")
-    akser.plot(xpoints, data_mergesort[list_order].values, label="Mergesort")
-    akser.plot(xpoints, data_mergesort_insert[list_order].values, label="Mergesort insert")
-    akser.plot(xpoints, data_numpy_sort[list_order].values, label="Numpy sort")
-    akser.plot(xpoints, data_python_sort[list_order].values, label="Python sort")
-    akser.plot(xpoints, data_quicksort_insert[list_order].values, label="Quicksort insert")
-    akser.plot(xpoints, data_quicksortiterative[list_order].values, label="Quicksort iterative")
+    if list_order == "sorted" or list_order == "reversed" or list_order == "random":
+        for n in enumerate(data):
+            akser.plot(xpoints, n[1][list_order].values, label=data_labels[n[0]])
+            list_order_label = list_order
+    else:
+        if list_order == "best_case":
+            orders = get_best_case(data)
+            list_order_label = "best case"
+        elif list_order == "worst_case":
+            orders = get_worst_case(data)
+            list_order_label = "worst case"
+        for n in enumerate(data):
+            order = orders[n[0]]
+            akser.plot(xpoints, n[1][order].values, label=data_labels[n[0]])
+        
     
     akser.set_yscale("log")
     akser.set_xlabel("List size (n)")
     akser.set_ylabel("Time (s)")
-    akser.set_title("Time usage for sorting algorithms on a list in "+list_order+" order")
+    akser.set_title("Time usage for sorting algorithms on a list in "+list_order_label+" order")
     akser.legend()
     
     plt.show()
 
 
+def get_best_case(data):
+    best_cases = []
+    for n in enumerate(data):
+        algo_orders = {"sorted": n[1]["sorted"][8], "reversed": n[1]["reversed"][8], "random": n[1]["random"][8]}
+        best_cases.append(max(algo_orders, key=algo_orders.get))
+    return best_cases
+
+
+def get_worst_case(data):
+    worst_cases = []
+    for n in enumerate(data):
+        algo_orders = {"sorted": n[1]["sorted"][8], "reversed": n[1]["reversed"][8], "random": n[1]["random"][8]}
+        worst_cases.append(min(algo_orders, key=algo_orders.get))
+    return worst_cases
+
+
 if __name__ == "__main__":
-    plot_time_datasize("sorted")
-    plot_time_datasize("reversed")
-    plot_time_datasize("random")
+    plot_time_datasize(data, data_labels, "sorted")
+    plot_time_datasize(data, data_labels, "reversed")
+    plot_time_datasize(data, data_labels, "random")
+    plot_time_datasize(data, data_labels, "best_case")
+    plot_time_datasize(data, data_labels, "worst_case")
     
