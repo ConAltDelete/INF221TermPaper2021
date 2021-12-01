032e118ccc9556d9eed27c66417dff282cfbbf77
Sindre Stokke <sindre.stokke@nmbu.no>
2021-12-01 14:18:25 +0100

    Legger til Mats sine forslag

--- code/python/plots.py
+++ code/python/plots.py
@@ -8,10 +8,11 @@ Created on Wed Nov  3 14:48:38 2021
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-folder = "../../data/csv_files/"
+folder = "../../data/csv_files_roy/"
 
-data = [pd.read_csv(folder+"bobble_sort.csv"),
+data = [pd.read_csv(folder+"bubble_sort.csv"),
         pd.read_csv(folder+"insertion_sort.csv"),
         pd.read_csv(folder+"mergesort.csv"),
         pd.read_csv(folder+"mergesort_insert.csv"),
@@ -30,24 +31,29 @@ def plot_time_datasize(data, data_labels, list_order):
     figur = plt.figure()
     akser = figur.add_subplot()
     
-    if list_order == "sorted" or list_order == "reversed" or list_order == "random":
+    if list_order in ["sorted","reversed","random"]:
         for n in enumerate(data):
-            akser.plot(xpoints, n[1][list_order].values, label=data_labels[n[0]])
+            akser.plot(xpoints, n[1][list_order].values, label=data_labels[n[0]], marker='o')
             list_order_label = list_order
-    else:
+    elif list_order in ["worst_case","best_case"]:
         if list_order == "best_case":
-            orders = get_best_case(data)
+            orders = get_best_or_worst_case(data, "best")
             list_order_label = "best case"
         elif list_order == "worst_case":
-            orders = get_worst_case(data)
+            orders = get_best_or_worst_case(data, "worst")
             list_order_label = "worst case"
         for n in enumerate(data):
             order = orders[n[0]]
-            akser.plot(xpoints, n[1][order].values, label=data_labels[n[0]])
+            akser.plot(xpoints, n[1][order].values, label=data_labels[n[0]], marker='o')
+    else:
+        raise ValueError("Not valid parameter, got " + list_order)
         
+    quadratic_plot(xpoints, akser)
+    #nlogn_plot(xpoints, akser)
+    
     
     akser.set_yscale("log")
-    akser.set_xlabel("List size (n)")
+    akser.set_xlabel("List size (2^n)")
     akser.set_ylabel("Time (s)")
     akser.set_title("Time usage for sorting algorithms on a list in "+list_order_label+" order")
     akser.legend()
@@ -55,20 +61,36 @@ def plot_time_datasize(data, data_labels, list_order):
     plt.show()
 
 
-def get_best_case(data):
-    best_cases = []
+def get_best_or_worst_case(data, best_or_worst):
+    """
+    Returns a list of each algorithm's best or worst case input list. Either "sorted", "reversed" or "random".
+    """
+    best_or_worst_cases = []
     for n in enumerate(data):
         algo_orders = {"sorted": n[1]["sorted"][8], "reversed": n[1]["reversed"][8], "random": n[1]["random"][8]}
-        best_cases.append(max(algo_orders, key=algo_orders.get))
-    return best_cases
+        if best_or_worst == "best":
+            best_or_worst_cases.append(max(algo_orders, key=algo_orders.get))
+        if best_or_worst == "worst":
+            best_or_worst_cases.append(min(algo_orders, key=algo_orders.get))
+    return best_or_worst_cases
 
 
-def get_worst_case(data):
-    worst_cases = []
-    for n in enumerate(data):
-        algo_orders = {"sorted": n[1]["sorted"][8], "reversed": n[1]["reversed"][8], "random": n[1]["random"][8]}
-        worst_cases.append(min(algo_orders, key=algo_orders.get))
-    return worst_cases
+def quadratic_plot(xpoints, akser):
+    akser.plot(xpoints, 4**xpoints, label="Quadratic", marker='o')
+    print(xpoints)
+    print(4**xpoints)
+
+
+def nlogn_plot(xpoints, akser):
+    akser.plot(xpoints, xpoints*math.log(xpoints), label="nlogn", marker='o')
+    print(xpoints)
+    print(2**xpoints)
+
+
+def linear_plot(xpoints, akser):
+    akser.plot(xpoints, xpoints, label="Linear", marker='o')
+    print(xpoints)
+    print(xpoints)
 
 
 if __name__ == "__main__":
