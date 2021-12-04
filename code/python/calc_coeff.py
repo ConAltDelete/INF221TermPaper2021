3cb58f0ace63c1272644d18ff6023d9ef8ac6150
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2021-12-04 14:44:01 +0100

    backup commit

--- code/python/calc_coeff.py
+++ code/python/calc_coeff.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
             "quicksort_insert",
             "quicksortIterative"
             ]
-    collected_data = ce.get_data("./data/csv_files_backup/",algol_list) #TODO: Dataen's x er log2()
+    collected_data = ce.get_data("./data/csv_files/",algol_list) #TODO: Dataen's x er log2()
 
     for i,key in enumerate(collected_data):
         collected_data[key]["data"][i][0] = 2**collected_data[key]["data"][i][0]
@@ -58,65 +58,100 @@ if __name__ == "__main__":
             lambda x: math.log2(x)
             ]
     algol_funcs = {
-            "numpy_sort": {"random":[funcs[2]]},
-            "python_sort": {"random":[funcs[3]]},
-            "insertion_sort": {"random":[funcs[2]]},
-            "quicksort": {"random":[funcs[2]]},
-            "mergesort": {"random":[funcs[3]]},
-            "cyclesort": {"random":[funcs[2]]},
-            "quicksort_insert": {"random":[funcs[2]]},
-            "bubble_sort": {"random":[funcs[2]]},
-            "mergesort_insert": {"random":[funcs[3]]}#,
-            #"quicksortIterative": {"random":[funcs[2]]}
+            "numpy_sort": {
+                "random":[funcs[3]],
+                "sorted":[funcs[1]],
+                "reversed":[funcs[2]]
+                },
+            "python_sort": {
+                "random":[funcs[3]],
+                "sorted":[funcs[1]],
+                "reversed":[funcs[3]]
+                },
+            "insertion_sort": {
+                "random":[funcs[2]],
+                "sorted":[funcs[1]],
+                "reversed":[funcs[2]]
+                },
+            "quicksort": {
+                "random":[funcs[2]],
+                "sorted":[funcs[3]],
+                "reversed":[funcs[2]]
+                },
+            "mergesort": {
+                "random":[funcs[3]],
+                "sorted":[funcs[3]],
+                "reversed":[funcs[3]]
+                },
+            "quicksort_insert": {
+                "random":[funcs[2]],
+                "sorted":[funcs[3]],
+                "reversed":[funcs[3]]
+                },
+            "bubble_sort": {
+                "random":[funcs[2]],
+                "sorted":[funcs[1]],
+                "reversed":[funcs[2]]
+                },
+            "mergesort_insert": {
+                "random":[funcs[3]],
+                "sorted":[funcs[1]],
+                "reversed":[funcs[3]]
+                }
             }
 
-    label = "random"
+    labels = ["sorted","random","reversed"]
 
     results = dict()
     
     print("\n:Calculating coeffs:\n")
 
-    for data in collected_data.keys() & algol_funcs.keys(): 
-        index_label = collected_data[data]["head"].index(label)
+    for data in collected_data.keys() & algol_funcs.keys():
+        print(data,end=": ")
+        for label in labels:
+            print(label,end=":\n")
+            index_label = collected_data[data]["head"].index(label)
 
-        data_fetch = [
+            data_fetch = [
                     [],
                     []
-                ]
-        for row in collected_data[data]["data"]:
-            data_fetch[0].append(2**row[0])
-            data_fetch[1].append(row[index_label])
+                    ]
+            for row in collected_data[data]["data"]:
+                data_fetch[0].append(2**row[0])
+                data_fetch[1].append(row[index_label])
         
-        if len(data_fetch) == 0:
-            continue
-        print(data, end = ": ")
-        print(result := abs(float(ce.mod_regrass(data_fetch,algol_funcs[data][label])[0,0])),end="-> trying estimation: ")
+            if len(data_fetch) == 0:
+                continue
 
-        results[data] = result
+            print(result := abs(float(ce.mod_regrass(data_fetch,algol_funcs[data][label])[0,0])),end="-> trying estimation: ")
+
+            results[data] = result
         
-        # O-claculation
+            # O-claculation
 
-        n0 = -1
-        scaler = 1
+            n0 = -1
+            scaler = 1
 
-        scale_lim = 100000
+            scale_lim = 100000
 
-        while scaler < scale_lim:
-            # print(n0,scaler)
-            list_a = list(map(lambda x: scaler*result*algol_funcs[data][label][0](float(x)),data_fetch[0]))
+            while scaler < scale_lim:
+                # print(n0,scaler)
+                list_a = list(map(lambda x: scaler*result*algol_funcs[data][label][0](float(x)),data_fetch[0]))
             #print(list_a,data_fetch[1])
-            if logic_comp(list_a,data_fetch[1],int("100",2),n0=n0):
-                break
-            elif n0 >= len(data_fetch[0])/2:
-                n0 = -1
-                scaler += 1
-            else:
-                n0 += 1
+                if logic_comp(list_a,data_fetch[1],int("100",2) if label == "reversed" else int("001",2),n0=n0):
+                    break
+                elif n0 >= len(data_fetch[0])/2:
+                    n0 = -1
+                    if label == "reversed":
+                        scaler += 1
+                    elif label == "sorted":
+                        scaler /= 2
+                    else:
+                        scaler 
+                else:
+                    n0 += 1
 
-        if scaler >= scale_lim:
-            print("estimation Failed!")
-        else:
-            print("estimation succsess! coff={}, n0={}".format(scaler*result,n0))
-            print("\testimated\tdata")
-            for a in zip(list_a,data_fetch[1]):
-                print("\t{}\t{}".format(a[0],a[1]))
+            if scaler >= scale_lim:
+                print("estimation Failed!")
+            else:
+                print("estimation succsess! coff={}, n0={}".format(scaler*result,n0))
